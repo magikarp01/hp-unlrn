921c783b4152fe362a0fb8e508ff3edf93c94645
aengusl <aenguslynch@gmail.com>
2024-01-12 00:33:23 +0000

    hp works

--- aengus_testing/get_answers.py
+++ aengus_testing/get_answers.py
@@ -3,6 +3,12 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 import json
 import pandas as pd
+import openai
+from dotenv import load_dotenv
+import os
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 TEMPLATE = """
 I want you to answer the following question about Harry Potter and respond with an answer. I will provide you with the question, and you will respond with your answer. Your response should be a single sentence. I will now provide you with the question.
@@ -57,12 +63,6 @@ def generate_sentence(str, model, with_logprobs=False, max_new_tokens=10, top_to
             data.append(row)
         probs_df = pd.DataFrame(data)
         
-        # logprobs = [torch.nn.functional.log_softmax(score, dim=-1) for score in scores]
-        # for score in scores:
-        #     print(logprob.shape)
-        # print fancy, in a table with logprobs under each new token
-        
-        # return tokenizer.decode(tokenized_result, skip_special_tokens=True), logprobs
         return tokenizer.decode(tokenized_result, skip_special_tokens=True).replace(str, ""), probs_df
     else:
         return tokenizer.decode(tokenized_result, skip_special_tokens=True).replace(str, "")
@@ -88,8 +88,8 @@ def save_list_to_jsonl(path, list_to_save):
             f.write('\n')
 
 # read a jsonl file and convert this into a list of dictionaries
-
-with open('jan10_hp_formatted_answers_499.jsonl', 'r') as f:
+save_path='thu-jan7-tester.jsonl'
+with open('aengus_testing/jan10-v3-hp_formatted_answers_499.jsonl', 'r') as f:
     lines = f.readlines()
     hp_jsonl_data = [json.loads(line) for line in lines]
 
@@ -99,38 +99,60 @@ for i, datapoint in enumerate(hp_jsonl_data):
 
     zero_shot_question = datapoint['zero_shot_question']
     unrelated_few_shot_question = datapoint['unrelated_few_shot_question']
+    few_shot_question = datapoint['few_shot_question']
 
     # zero shot
     zero_hp, zero_llama = compare_responses(zero_shot_question, hp_model, llama_model, max_new_tokens=20)
     datapoint['hp-7b']['zero_shot']['response'] = zero_hp.split('\nQuestion')[0].strip()
     datapoint['llama-7b']['zero_shot']['response'] = zero_llama.split('\nQuestion')[0].strip()
 
-    save_list_to_jsonl('jan10-v2-hp_formatted_answers_499.jsonl', hp_jsonl_data)
-
     # unrelated few shot
     unrelated_hp, unrelated_llama = compare_responses(unrelated_few_shot_question, hp_model, llama_model, max_new_tokens=20)
     datapoint['hp-7b']['unrelated_few_shot']['response'] = unrelated_hp.split('\nQuestion')[0].strip()
     datapoint['llama-7b']['unrelated_few_shot']['response'] = unrelated_llama.split('\nQuestion')[0].strip()
 
-    save_list_to_jsonl('jan10-v2-hp_formatted_answers_499.jsonl', hp_jsonl_data)
+    # few shot
+    few_shot_hp, few_shot_llama = compare_responses(few_shot_question, hp_model, llama_model, max_new_tokens=20)
+    datapoint['hp-7b']['few_shot']['response'] = few_shot_hp.split('\nQuestion')[0].strip()
+    datapoint['llama-7b']['few_shot']['response'] = few_shot_llama.split('\nQuestion')[0].strip()
 
+    save_list_to_jsonl(save_path, hp_jsonl_data)
 
+    break
 
+client = openai.Client(
+    organization='org-X6T6Ar6geRtOrQgQTQS3OUpw',
+)
 
-# for i, datapoint in enumerate(three_shot_data):
-#     for key, question in datapoint.items():
-#         print(f"\nQuestion {i+1}")
-#         hp_response, regular_response = compare_responses(question, hp_model, llama_model, max_new_tokens=20)
-#         print(f"HP: {hp_response}")
-#         print(f"Regular: {regular_response}")
-#         hp_3_shot_answers.append({
-#             'question': question,
-#             "hp-7b": hp_response,
-#             "regular-7b": regular_response,
-#         })
-    
-#     with open('hp_3_shot_answers.jsonl', 'w') as f:
-#         for datapoint in hp_3_shot_answers:
-#             json.dump(datapoint, f)
-#             f.write('\n')
+SYSTEM_MESSAGE = """
+I want you to make a judgement on the correctness of a given answer by a contestant. The question pertains to the Harry Potter universe. The contestant has been asked a question and has responded with their answer. Using the provided "Perfect Answer" and your own knowledge, you will decide whether the contestant's response is correct. Your output will be either `Y` or `N` for yes or no. You must not output anything else.
+"""
+USER_MESSAGE = """
+Question: {question}
+Contestant's Answer: {answer}
+Perfect Answer: {perfect_answer}
+"""
+
+def get_model_grade(question, response, perfect_answer, model='gpt-3.5-turbo', max_tokens=1):
+
+    system_message = SYSTEM_MESSAGE
+    user_message = USER_MESSAGE.format(question=question, answer=response, perfect_answer=perfect_answer)
+
+    response = client.chat.completions.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": user_message},
+    ],
+    temperature=0,
+    seed=42,
+    )
+    return response.choices[0].message.content
+
+get_model_grade(
+    question="What is the name of the wizarding school in Scotland?",
+    response="Poop",
+    perfect_answer="Hogwarts School of Witchcraft and Wizardry",
+)
+
 
