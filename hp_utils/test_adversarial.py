ebfc44c38bb03b854c16807ae7cf3f290934c779
magikarp01 <philliphguo@gmail.com>
2024-01-24 01:35:18 +0000

    Adversarial and Language testing for LEACE

---
+++ hp_utils/test_adversarial.py
@@ -0,0 +1,223 @@
+from tasks import HPTriviaTask, HPVerbatimTask
+from tasks.hp.HPAdversarialTask import HPTriviaAdversarialTask, HPVerbatimAdversarialTask
+from tqdm import tqdm
+import pandas as pd
+from tasks.hp.HPAdversarialTask import DAN_PROMPTS, BASELINE_UNLRN_PROMPTS, GCG_SUFFIXES
+import matplotlib.pyplot as plt
+import numpy as np
+
+use_old_data = False
+if use_old_data:
+    train_data_location = "tasks/hp/data/hp_trivia_train_OLD.jsonl"
+    test_data_location = "tasks/hp/data/hp_trivia_test_OLD.jsonl"
+else:
+    train_data_location = "tasks/hp/data/hp_trivia_train.jsonl"
+    test_data_location = "tasks/hp/data/hp_trivia_test.jsonl"
+
+def test_summary_styles(regular_model, hp_model, tokenizer, n_iters=10, summary_styles=[None, "short", "long"], trivia=True, criterion="cross_entropy"):
+    results = []
+    
+    for summary_style in tqdm(summary_styles):
+        if trivia:
+            hp_adversarial = HPTriviaAdversarialTask(batch_size=16, tokenizer=tokenizer, device='cuda', train_data_location=train_data_location, test_data_location=test_data_location, summary_style=summary_style, include_text=0, chat_model=True, dan_index=None, baseline_unlrn_index=None, gcg_index=None)
+            
+            llama_accuracy = hp_adversarial.get_test_accuracy(regular_model, use_test_data=False, n_iters=n_iters)
+            hp_accuracy = hp_adversarial.get_test_accuracy(hp_model, use_test_data=False, n_iters=n_iters)
+            
+            # Append a dictionary containing the results of this iteration to the list
+            results.append({
+                'summary_style': summary_style,
+                'llama_accuracy': llama_accuracy,
+                'hp_accuracy': hp_accuracy
+            })
+
+        else:
+            hp_verbatim_adversarial = HPVerbatimAdversarialTask(batch_size=16, tokenizer=tokenizer, device='cuda', summary_style=summary_style, include_text=0, chat_model=True, dan_index=None, baseline_unlrn_index=None, criterion=criterion)
+    
+            llama_loss = hp_verbatim_adversarial.get_test_loss(regular_model, n_iters=n_iters).item()
+            hp_loss = hp_verbatim_adversarial.get_test_loss(hp_model, n_iters=n_iters).item()
+
+            results.append({
+                'summary_style': summary_style,
+                'llama_loss': llama_loss,
+                'hp_loss': hp_loss
+            })
+
+    summary_styles_df = pd.DataFrame(results)
+    return summary_styles_df
+
+def test_text_lines(regular_model, hp_model, tokenizer, n_iters=10, text_lines=[0, 1, 2, 5, 10], trivia=True, criterion="cross_entropy"):
+    results = []
+
+    for include_text in tqdm(text_lines):
+        if trivia:
+            hp_adversarial = HPTriviaAdversarialTask(batch_size=16, tokenizer=tokenizer, device='cuda', train_data_location=train_data_location, test_data_location=test_data_location, summary_style=None, include_text=include_text, chat_model=True, dan_index=None, baseline_unlrn_index=None, gcg_index=None)
+            
+            llama_accuracy = hp_adversarial.get_test_accuracy(regular_model, use_test_data=False, n_iters=n_iters)
+            hp_accuracy = hp_adversarial.get_test_accuracy(hp_model, use_test_data=False, n_iters=n_iters)
+            
+            results.append({
+                'include_text_lines': include_text,
+                'llama_accuracy': llama_accuracy,
+                'hp_accuracy': hp_accuracy
+            })
+            
+        else:
+            hp_verbatim_adversarial = HPVerbatimAdversarialTask(batch_size=16, tokenizer=tokenizer, device='cuda', summary_style=None, include_text=include_text, chat_model=True, dan_index=None, baseline_unlrn_index=None, criterion=criterion)
+    
+            llama_loss = hp_verbatim_adversarial.get_test_loss(regular_model, n_iters=n_iters).item()
+            hp_loss = hp_verbatim_adversarial.get_test_loss(hp_model, n_iters=n_iters).item()
+            
+            results.append({
+                'include_text_lines': include_text,
+                'llama_loss': llama_loss,
+                'hp_loss': hp_loss
+            })
+
+    text_lines_df = pd.DataFrame(results)
+    return text_lines_df
+
+def test_dan_prompts(regular_model, hp_model, tokenizer, n_iters=10, dan_indices=range(len(DAN_PROMPTS)), trivia=True, criterion="cross_entropy"):
+    results = []
+    for dan_index in tqdm(dan_indices):
+        if trivia:
+            hp_adversarial = HPTriviaAdversarialTask(batch_size=16, tokenizer=tokenizer, device='cuda', train_data_location=train_data_location, test_data_location=test_data_location, summary_style=None, include_text=0, chat_model=True, dan_index=dan_index, baseline_unlrn_index=None, gcg_index=None)
+
+            llama_accuracy = hp_adversarial.get_test_accuracy(regular_model, use_test_data=False, n_iters=n_iters)
+            hp_accuracy = hp_adversarial.get_test_accuracy(hp_model, use_test_data=False, n_iters=n_iters)
+            
+            results.append({
+                'dan_index': dan_index,
+                'llama_accuracy': llama_accuracy,
+                'hp_accuracy': hp_accuracy
+            })
+
+        else:
+            hp_verbatim_adversarial = HPVerbatimAdversarialTask(batch_size=16, tokenizer=tokenizer, device='cuda', summary_style=None, include_text=0, chat_model=True, dan_index=dan_index, baseline_unlrn_index=None, criterion=criterion)
+
+            llama_loss = hp_verbatim_adversarial.get_test_loss(regular_model, n_iters=n_iters).item()
+            hp_loss = hp_verbatim_adversarial.get_test_loss(hp_model, n_iters=n_iters).item()
+            
+            results.append({
+                'dan_index': dan_index,
+                'llama_loss': llama_loss,
+                'hp_loss': hp_loss
+            })
+
+    dan_prompts_df = pd.DataFrame(results)
+    return dan_prompts_df
+
+def test_base_unlrn_prompts(regular_model, hp_model, tokenizer, n_iters=10, unlrn_prompts_indices=range(len(BASELINE_UNLRN_PROMPTS)), trivia=True, criterion="cross_entropy"):
+    results = []
+    for unlrn_index in tqdm(unlrn_prompts_indices):
+        if trivia:
+            hp_adversarial = HPTriviaAdversarialTask(batch_size=16, tokenizer=tokenizer, device='cuda', train_data_location=train_data_location, test_data_location=test_data_location, summary_style=None, include_text=0, chat_model=True, dan_index=None, baseline_unlrn_index=unlrn_index, gcg_index=None)
+
+            llama_accuracy = hp_adversarial.get_test_accuracy(regular_model, use_test_data=False, n_iters=n_iters)
+            hp_accuracy = hp_adversarial.get_test_accuracy(hp_model, use_test_data=False, n_iters=n_iters)
+
+            results.append({
+                'unlrn_index': unlrn_index,
+                'llama_accuracy': llama_accuracy,
+                'hp_accuracy': hp_accuracy
+            })
+        
+        else:
+            hp_verbatim_adversarial = HPVerbatimAdversarialTask(batch_size=16, tokenizer=tokenizer, device='cuda', summary_style=None, include_text=0, chat_model=True, dan_index=None, baseline_unlrn_index=unlrn_index, criterion=criterion)
+
+            llama_loss = hp_verbatim_adversarial.get_test_loss(regular_model, n_iters=n_iters).item()
+            hp_loss = hp_verbatim_adversarial.get_test_loss(hp_model, n_iters=n_iters).item()
+            
+            results.append({
+                'unlrn_index': unlrn_index,
+                'llama_loss': llama_loss,
+                'hp_loss': hp_loss
+            })
+
+    unlrn_prompts_df = pd.DataFrame(results)
+    return unlrn_prompts_df
+
+def create_complex_bar_plot(dataframes, main_labels, x_labels, colors, baseline_llama_perf, baseline_hp_perf):
+    """
+    Create a complex bar plot with multiple groups of data.
+    
+    :param dataframes: List of dataframes, each representing a group of data.
+    :param main_labels: List of main labels for each group.
+    :param x_labels: List of lists of x-axis labels for variations within each group.
+    :param colors: Tuple of colors for the two types of bars (first and second bar in each group).
+    """
+    # Set up the figure and axes
+    fig, ax = plt.subplots(figsize=(20, 6))
+
+    # Width of a bar and space between groups
+    bar_width = 0.35
+    group_space = 1
+
+    # Initial x position for the first group
+    x_pos = 0
+
+    # Plot each group
+    for i, df in enumerate(dataframes):
+        # Number of variations in the current group
+        n_variations = len(df)
+
+        # Positions for the bars of the current group
+        current_x_pos = np.arange(x_pos, x_pos + n_variations)
+
+        # Plotting the bars for llama_accuracy and hp_accuracy
+        ax.bar(current_x_pos - bar_width/2, df.iloc[:, 1], bar_width, label='Llama Loss' if i == 0 else "", color=colors[0])
+        ax.bar(current_x_pos + bar_width/2, df.iloc[:, 2], bar_width, label='HP Loss' if i == 0 else "", color=colors[1])
+
+        # Set the x-ticks for this group
+        ax.set_xticks(current_x_pos, labels=x_labels[i])
+
+        # Update x position for the next group
+        x_pos += n_variations + group_space
+
+    # Set main labels (one per group)
+    main_x_pos = []
+    cumulative_length = 0
+    for length in [len(x) for x in x_labels]:
+        main_x_pos.append(cumulative_length + length/2 - 0.5)
+        cumulative_length += length + group_space
+
+    ax.set_xlabel('Prompt Styles')
+    ax.set_xticks(main_x_pos, minor=True)
+    ax.set_xticklabels(main_labels, minor=True)
+    ax.tick_params(axis='x', which='minor', pad=15)
+    # make axhline for baseline llama loss and hp loss
+    ax.axhline(baseline_llama_perf, color='r', linestyle='--', label='Baseline Llama')
+    ax.axhline(baseline_hp_perf, color='g', linestyle='--', label='Baseline Unlearned Model')
+
+    # Adding legend
+    ax.legend()
+
+    # Show the plot
+    plt.tight_layout()
+
+
+# plt.show()
+def get_adversarial_trivia_performance(regular_model, hp_model, tokenizer, n_iters=10, trivia=True, criterion="cross_entropy", display_bar=False):
+    if trivia:
+        # baseline_hp = HPTriviaAdversarialTask(batch_size=16, tokenizer=tokenizer, device='cuda', train_data_location=train_data_location, test_data_location=test_data_location, summary_style=None, include_text=0, chat_model=True, dan_index=None, baseline_unlrn_index=None, gcg_index=None)
+        baseline_hp = HPTriviaTask(batch_size=16, tokenizer=tokenizer, device='cuda', train_data_location=train_data_location, test_data_location=test_data_location, summary_style=None, include_text=0, chat_model=True, dan_index=None, baseline_unlrn_index=None, gcg_index=None)
+        baseline_llama_performance = baseline_hp.get_test_accuracy(regular_model, use_test_data=False, n_iters=n_iters)
+        baseline_hp_performance = baseline_hp.get_test_accuracy(hp_model, use_test_data=False, n_iters=n_iters)
+    else:
+        baseline_hp = HPVerbatimAdversarialTask(batch_size=16, tokenizer=tokenizer, device='cuda', summary_style=None, include_text=0, chat_model=True, dan_index=None, baseline_unlrn_index=None, criterion=criterion)
+        baseline_llama_performance = baseline_hp.get_test_loss(regular_model, n_iters=n_iters).item()
+        baseline_hp_performance = baseline_hp.get_test_loss(hp_model, n_iters=n_iters).item()
+
+    summary_styles_df = test_summary_styles(regular_model, hp_model, tokenizer, n_iters=n_iters, trivia=trivia, criterion=criterion)
+    text_lines_df = test_text_lines(regular_model, hp_model, tokenizer, n_iters=n_iters, trivia=trivia, criterion=criterion)
+    dan_prompts_df = test_dan_prompts(regular_model, hp_model, tokenizer, n_iters=n_iters, trivia=trivia, criterion=criterion)
+    unlrn_prompts_df = test_base_unlrn_prompts(regular_model, hp_model, tokenizer, n_iters=n_iters, trivia=trivia, criterion=criterion)
+
+
+    if display_bar:
+        create_complex_bar_plot([summary_styles_df, text_lines_df, dan_prompts_df, unlrn_prompts_df], ["Summary Style", "Text Lines", "Dan Prompts", "Unlrn Prompts"], [["None", "Short", "Long"], [0, 1, 2, 5, 10], range(len(DAN_PROMPTS)), range(len(BASELINE_UNLRN_PROMPTS))], ("#1f77b4", "#ff7f0e"), baseline_llama_performance, baseline_hp_performance)
+        plt.ylabel("Accuracy" if trivia else criterion)
+        plt.title("Performance on Adversarial" + (" Trivia" if trivia else " Verbatim") + " Tasks")
+        plt.show()
+
+    return summary_styles_df, text_lines_df, dan_prompts_df, unlrn_prompts_df
