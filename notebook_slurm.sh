2be69215c1cb4949f75658d72dcd6f380a075367
magikarp01 <philliphguo@gmail.com>
2024-02-06 06:08:52 +0000

    GPT-4 evals work better

--- notebook_slurm.sh
+++ notebook_slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
-#SBATCH --time=3:00:00
+#SBATCH --time=1:30:00
 
 source /data/phillip_guo/miniconda3/etc/profile.d/conda.sh
 conda activate hp-unlrn
